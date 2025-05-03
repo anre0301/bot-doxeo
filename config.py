f09e4cb59b4170cc745df14e22d2d1a6031dcbc9
anre0301 <juliomedina030122@gmail.com>
2025-05-03 14:57:26 -0500

    Modifique el API

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ API_ID = 23810582         # Este es tu API_ID
 API_HASH = '079750c767b2d4154acfff724a1a6b6e'   # Este es tu API_HASH
 
 # Configuración de Telegram Bot (para bot)
-BOT_TOKEN = '7573823761:AAEqnPZFvQwzEVMqgUQsPGi9K29hDla2hbs'  # El token de tu bot de Telegram
+BOT_TOKEN = '7573823761:AAF4k9vEtP1jbhe6RQqZsnVyD-uZX5PeEVQ'  # El token de tu bot de Telegram
 
 
